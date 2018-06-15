6992251114cfe98e2598a459700323aa68fb1cc2
Q4062 <Q4062@QKDSK301.Corp.qualitykiosk.com>
2018-06-15 11:08:43 +0530

    ok

---
+++ src/NewPackage/NewClass.java
@@ -0,0 +1,11 @@
+package NewPackage;
+
+public class NewClass {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		System.out.println("Integartion of tools");
+	}
+
+}
